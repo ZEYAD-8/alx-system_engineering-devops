06b40da78ed55c3bd15e3a66a1b58e7946e1b0e1
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-29 23:00:03 +0200

    Task 1: Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# installs a package 
+# installs the flask package 
 
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command  => '/usr/bin/pip3 install Flask==2.1.0',
+  provider => 'shell',
+  require  => Package['python3-pip'],
 }
