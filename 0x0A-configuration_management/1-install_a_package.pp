1dd3b0b25b51c575400c0f316bc41c2a6587c013
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-29 23:03:10 +0200

    Task 1: Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command  => '/usr/bin/pip3 install Flask==2.1.0',
+  command  => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   provider => 'shell',
   require  => Package['python3-pip'],
 }
