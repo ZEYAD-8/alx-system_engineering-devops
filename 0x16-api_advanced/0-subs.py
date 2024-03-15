72c7df58a4e1322256162ad1c76224ce47c5b8a9
ZEYAD-8 <zeyadosama15@gmail.com>
2024-03-15 16:14:57 +0200

    Number of Subscribers of a Subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """Returns the number of subs, 0 if the given subreddit is invalid."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyBot/0.1'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
