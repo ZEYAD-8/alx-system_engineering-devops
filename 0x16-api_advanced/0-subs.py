c57113ddc8a810f820f8badc3890abb29e216989
ZEYAD-8 <zeyadosama15@gmail.com>
2024-03-15 15:46:51 +0200

    Number of Subscribers of a Subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-
+"""
+Function that queries the Reddit API and
+returns the number of subscribers.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """Returns the number of subs, 0 if the given subreddit is invalid."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyBot/0.1'}
     response = requests.get(url, headers=headers)
